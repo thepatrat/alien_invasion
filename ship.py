dcb7895472cbfd0c7412ea4dc9526ff95a6eaea5
thepatrat <71664904+thepatrat@users.noreply.github.com>
2021-03-18 20:17:20 +0100

    first commit

--- ship.py
+++ ship.py
@@ -5,7 +5,25 @@ class Ship:
 
     def __init__(self, ai_game):
         """ Initialize the shi and set its starting position. """
+        # screen is assigned as an attribute to the ship
         self.screen = ai_game.screen
+        # we get the screens rect and assign it as an attribute
+        # allows to place the ship in the screen
         self.screen_rect = ai_game.screen.get_rect()
 
-        #load
+        # Load the ship image and assign in to self.image
+        self.image = pygame.image.load('images/ship.png')
+        # assign this image to a rectangle using get_rect 
+        # and assigning it to self. rect
+        self.rect = self.image.get_rect()
+
+        # Start each new ship at the bottom center of the screen.
+        # we are matching the value of self.rect mitbottom
+        # to the midbottom attribute of screens rect
+    self.rect.midbottom = self.screen_rect.midbottom
+
+# draws the image to the screen at the position 
+# specified by self.rect
+def blitme(self):
+    """Draw the ship at its current location."""
+    self.screen.blit(self.image, self.rect)
--- spaceship_facelift.py
+++ spaceship_facelift.py
@@ -2,18 +2,22 @@ import sys
 import pygame
 from settings import Settings
 
+
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
     def __init__(self):
         """Initialize the game and create game resources."""
         pygame.init()
+        # gives the attribute setting to the instance and
+        # uses the method Settings defined in the settings.py
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width,self.settings.screen_height))
+        self.screen = pygame.display.set_mode((
+            self.settings.screen_width,
+            self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
-        #Set the background color.
+        # Set the background color.
         self.bg_color = self.settings.bg_color
     
     def run_game(self):
@@ -24,13 +28,16 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            # Redraw the screen during each pass through the loop
+            # Redraw the screen during each pass through the
+            # loop
             self.screen.fill(self.settings.bg_color)
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
 
+
 if __name__ == '__main__':
     # Make a game instance, and run the game.
+    # game instance is called ai
     ai = AlienInvasion()
     ai.run_game()
